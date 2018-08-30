475ec7943723433f9c08ef2ce12dec6d89aa3bf6
varun39 <v.goyal39@gmail.com>
2018-08-30 23:08:52 +0530

    assignment11

---
+++ assignment11.py
@@ -0,0 +1,1 @@
+# Q-1import res=input("Enter an E-mail")m=re.findall('^[0-9A-Za-z][\w_]*[@](gmail.com|yahoo.com)',s)if m:    print("Valid email")else:    print("Invalid")# Q-2import res=input("Enter a mobile number with country code")m=re.findall('^[+]91[6-9][0-9]{9}',s)if m:    print("Indian Number")else:    print("Not Indian")
